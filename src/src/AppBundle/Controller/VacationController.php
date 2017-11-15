77397e441202e5497cf81dbfc3f1b65065f6ed53
Rene Schmusch <Rene.Schmusch@digitalrepublic.com>
2017-11-15 16:18:09 +0100

    Update

---
+++ src/src/AppBundle/Controller/VacationController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class VacationController extends Controller
+{
+
+    /**
+     * @Route("/vacation", name="vacation")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('vacation.html.twig', [
+            //'debug' => $this->getUser(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/vacation", name="vacation")
+     */
+   public function FormAction()
+   {
+
+       $form = $this->createFormBuilder()
+           ->add('Date', DateType::class, [
+               'attr' => [
+                   'class' => 'form-control input-inline datepicker',
+                   'data-provide' => 'datepicker',
+                   'data-date-format' => 'dd-mm-yyyy',
+                   'daysOfWeekHighlighted' => "1",
+                   'daysOfWeekDisabled' => '0,6'
+               ]
+           ])
+           ->getForm();
+
+       return $this->render('vacation.html.twig', ['form' => $form->createView()]);
+
+    }
+}
